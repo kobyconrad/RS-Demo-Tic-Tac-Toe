a2e0518f1d1b8f5011314327af67f16893c9ff6c
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-02-01 16:22:09 -0800

    Finished collaborative tic tac toe

--- components/gameBlock.js
+++ components/gameBlock.js
@@ -1,12 +1,23 @@
 import React from "react";
 
-function GameBlock() {
+function GameBlock(props) {
   return (
-    <div>
+    <div onClick={props.onClick}>
+      {props.selection}
       <style jsx>{`
-        background-color: blue;
+        background-color: white;
         width: 200px;
         height: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-style: solid;
+        border-color: black;
+        border-width: ${props.borderWidth};
+        color: black;
+        font-size: 100px;
+        font-wieght: strong;
+        font-family: Arial;
       `}</style>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -10,29 +10,219 @@ const client = new RoomService({
 export default () => {
   const [state, setState] = useRoomService(client, "tic-tac-toe");
 
-  function onClick() {
-    setDoc(prevDoc => {
-      prevDoc.number = Math.floor(Math.random() * 100);
+  function onClickOne() {
+    setState(state => {
+      state.one = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function onClickTwo() {
+    setState(state => {
+      state.two = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function onClickThree() {
+    setState(state => {
+      state.three = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function onClickFour() {
+    setState(state => {
+      state.four = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function onClickFive() {
+    setState(state => {
+      state.five = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function onClickSix() {
+    setState(state => {
+      state.six = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function onClickSeven() {
+    setState(state => {
+      state.seven = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function onClickEight() {
+    setState(state => {
+      state.eight = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function onClickNine() {
+    setState(state => {
+      state.nine = state.turn;
+    });
+    if (state.turn === "X") {
+      setState(state => {
+        state.turn = "O";
+      });
+    } else {
+      setState(state => {
+        state.turn = "X";
+      });
+    }
+  }
+
+  function reset() {
+    setState(state => {
+      state.one = "";
+      state.two = "";
+      state.three = "";
+      state.four = "";
+      state.five = "";
+      state.six = "";
+      state.seven = "";
+      state.eight = "";
+      state.nine = "";
     });
   }
 
   return (
     <div>
       <h1>Collaborative Tic Tac Toe Demo</h1>
+      <button onClick={reset}>Reset</button>
+      <div>Current Turn = {state.turn}</div>
       <div className="gameContainer">
-        <GameBlock />
-        <GameBlock />
-        <GameBlock />
-        <GameBlock />
-        <GameBlock />
+        <GameBlock
+          selection={state.one}
+          onClick={onClickOne}
+          borderWidth="0px 5px 5px 0px"
+        />
+        <GameBlock
+          selection={state.two}
+          onClick={onClickTwo}
+          borderWidth="0px 5px 5px 5px"
+        />
+        <GameBlock
+          selection={state.three}
+          onClick={onClickThree}
+          borderWidth="0px 0px 5px 5px"
+        />
+        <GameBlock
+          selection={state.four}
+          onClick={onClickFour}
+          borderWidth="5px 5px 5px 0px"
+        />
+        <GameBlock
+          selection={state.five}
+          onClick={onClickFive}
+          borderWidth="5px"
+        />
+        <GameBlock
+          selection={state.six}
+          onClick={onClickSix}
+          borderWidth="5px 0px 5px 5px"
+        />
+        <GameBlock
+          selection={state.seven}
+          onClick={onClickSeven}
+          borderWidth="5px 5px 0px 0px"
+        />
+        <GameBlock
+          selection={state.eight}
+          onClick={onClickEight}
+          borderWidth="5px 5px 0px 5px"
+        />
+        <GameBlock
+          selection={state.nine}
+          onClick={onClickNine}
+          borderWidth="5px 0px 0px 5px"
+        />
       </div>
       <style jsx>{`
         .gameContainer {
-          height: 600px;
-          width: 600px;
+          display: flex;
+          flex-wrap: wrap;
+          height: 620px;
+          width: 620px;
           background-color: red;
         }
       `}</style>
     </div>
   );
 };
+
+// on click pushes in a state into the component display
+// state.turn rotates X or O
